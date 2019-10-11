b6285a0a5d9691eff65e7d9eacabbe4456e77262
Simon Blandford <simon@bkconnect.net>
2019-10-11 15:54:25 +0100

    Changed time format to human readable

--- timedlines.go
+++ timedlines.go
@@ -56,7 +56,7 @@ func main() {
 	reader := bufio.NewReader(inFile)
 	writer := bufio.NewWriter(outFile)
 
-	reLine := regexp.MustCompile(`^([0-9]+)(:)(.*)`)
+	reLine := regexp.MustCompile(`^([0-9]+):([0-9]+):([0-9]+).([0-9]+):(.*)`)
 
 	for {
 		text, err := reader.ReadString('\n')
@@ -73,25 +73,36 @@ func main() {
 					continue
 				}
 				// Fetch the timecode
-				timePassedText := reLine.ReplaceAllString(foundLine, "$1")
-				timePassed, err := strconv.ParseUint(timePassedText, 10, 64)
-				if err != nil {
-					log.Printf("Unable to parse time (%s, %d) on line : %s", timePassedText, timePassed, text)
+				timePassedHoursText := reLine.ReplaceAllString(foundLine, "$1")
+				timePassedMinutesText := reLine.ReplaceAllString(foundLine, "$2")
+				timePassedSecondsText := reLine.ReplaceAllString(foundLine, "$3")
+				timePassedMilliSecondsText := reLine.ReplaceAllString(foundLine, "$4")
+				timePassedHours, err1 := strconv.ParseUint(timePassedHoursText, 10, 64)
+				timePassedMinutes, err2 := strconv.ParseUint(timePassedMinutesText, 10, 64)
+				timePassedSeconds, err3 := strconv.ParseUint(timePassedSecondsText, 10, 64)
+				timePassedMilliSeconds, err4 := strconv.ParseUint(timePassedMilliSecondsText, 10, 64)
+				if err1 != nil || err2 != nil || err3 != nil || err4 != nil {
+					log.Printf("Unable to parse time  : %s", text)
 					continue
 				}
-
+				timePassedMs := timePassedMilliSeconds + (timePassedSeconds * 1000) + (timePassedMinutes * 1000 * 60) + (timePassedHours * 1000 * 3600)
+				timePassed := timePassedMs * 1000000
 				// Wait until time to print
 				for timePassed > uint64(time.Since(timeStart)) {
 					time.Sleep(sleepTimeMs * time.Millisecond)
 				}
 
 				// Fetch the line
-				outText := reLine.ReplaceAllString(foundLine, "$3")
+				outText := reLine.ReplaceAllString(foundLine, "$5")
 
 				fmt.Fprintf(writer, "%s\n", outText)
 			} else {
-				timePassed := time.Since(timeStart)
-				fmt.Fprintf(writer, "%d:%s", timePassed, text)
+				timePassedMs := time.Since(timeStart) / 1000000
+				timePassedMilliSeconds := timePassedMs % 100
+				timePassedSeconds := (timePassedMs / 1000) % 60
+				timePassedMinutes := (timePassedMs / (60 * 1000)) % 60
+				timePassedHours := timePassedMs / (60 * 60 * 1000)
+				fmt.Fprintf(writer, "%02d:%02d:%02d.%03d:%s", timePassedHours, timePassedMinutes, timePassedSeconds, timePassedMilliSeconds, text)
 			}
 			writer.Flush()
 		}
