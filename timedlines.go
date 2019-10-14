ce18dd062479d236dd68e06e0eb149af162706d4
Simon Blandford <simon@onepointltd.com>
2019-10-14 09:28:18 +0100

    Added optional time offset parameter

--- timedlines.go
+++ timedlines.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -15,6 +16,28 @@ import (
 
 const sleepTimeMs = 100
 
+func timeToMs(timeText string) (int64, error) {
+	reTime := regexp.MustCompile(`^(-?)([0-9]+):([0-9]+):([0-9]+).([0-9]+)$`)
+	// Translate sign
+	timeCodeSign := int64(1)
+	if reTime.ReplaceAllString(timeText, "$1") == "-" {
+		timeCodeSign = -1
+	}
+	// Translate timecode to mS
+	timeCodeHoursText := reTime.ReplaceAllString(timeText, "$2")
+	timeCodeMinutesText := reTime.ReplaceAllString(timeText, "$3")
+	timeCodeSecondsText := reTime.ReplaceAllString(timeText, "$4")
+	timeCodeMilliSecondsText := reTime.ReplaceAllString(timeText, "$5")
+	timeCodeHours, err1 := strconv.ParseInt(timeCodeHoursText, 10, 64)
+	timeCodeMinutes, err2 := strconv.ParseInt(timeCodeMinutesText, 10, 64)
+	timeCodeSeconds, err3 := strconv.ParseInt(timeCodeSecondsText, 10, 64)
+	timeCodeMilliSeconds, err4 := strconv.ParseInt(timeCodeMilliSecondsText, 10, 64)
+	if err1 != nil || err2 != nil || err3 != nil || err4 != nil {
+		return 0, errors.New("Unable to parse time as hh:mm:ss.ms format : " + timeText)
+	}
+	return (timeCodeMilliSeconds + (timeCodeSeconds * 1000) + (timeCodeMinutes * 1000 * 60) + (timeCodeHours * 1000 * 3600)) * timeCodeSign, nil
+}
+
 func main() {
 	timeStart := time.Now()
 
@@ -26,13 +49,22 @@ func main() {
 	}()
 
 	if len(os.Args) < 4 || (os.Args[1] != "encode" && os.Args[1] != "decode") {
-		log.Fatal("Usage: ", os.Args[0], " encode/decode <infile or -> <outfile or ->")
+		log.Fatal("Usage: ", os.Args[0], " encode/decode <infile | -> <outfile | -> [offset hh:mm:ss.ms]")
 	}
 
 	decode := os.Args[1] == "decode"
 
 	inFilePath := os.Args[2]
 	outFilePath := os.Args[3]
+
+	offSetMs := int64(0)
+	if len(os.Args) == 5 {
+		var err error
+		offSetMs, err = timeToMs(os.Args[4])
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 
 	inFile := os.Stdin
 	if inFilePath != "-" {
@@ -56,8 +88,6 @@ func main() {
 	reader := bufio.NewReader(inFile)
 	writer := bufio.NewWriter(outFile)
 
-	reLine := regexp.MustCompile(`^([0-9]+):([0-9]+):([0-9]+).([0-9]+):(.*)`)
-
 	for {
 		text, err := reader.ReadString('\n')
 		if err == io.EOF {
@@ -66,6 +96,7 @@ func main() {
 
 		if len(text) > 0 {
 			if decode {
+				reLine := regexp.MustCompile(`^(-?)([0-9]+:[0-9]+:[0-9]+.[0-9]+):(.*)`)
 				//Check line is in expected format
 				foundLine := reLine.FindString(text)
 				if len(foundLine) == 0 {
@@ -73,36 +104,32 @@ func main() {
 					continue
 				}
 				// Fetch the timecode
-				timePassedHoursText := reLine.ReplaceAllString(foundLine, "$1")
-				timePassedMinutesText := reLine.ReplaceAllString(foundLine, "$2")
-				timePassedSecondsText := reLine.ReplaceAllString(foundLine, "$3")
-				timePassedMilliSecondsText := reLine.ReplaceAllString(foundLine, "$4")
-				timePassedHours, err1 := strconv.ParseUint(timePassedHoursText, 10, 64)
-				timePassedMinutes, err2 := strconv.ParseUint(timePassedMinutesText, 10, 64)
-				timePassedSeconds, err3 := strconv.ParseUint(timePassedSecondsText, 10, 64)
-				timePassedMilliSeconds, err4 := strconv.ParseUint(timePassedMilliSecondsText, 10, 64)
-				if err1 != nil || err2 != nil || err3 != nil || err4 != nil {
-					log.Printf("Unable to parse time  : %s", text)
+				timeCodeMs, err := timeToMs(reLine.ReplaceAllString(foundLine, "$2"))
+				if err != nil {
+					log.Println(err)
 					continue
 				}
-				timePassedMs := timePassedMilliSeconds + (timePassedSeconds * 1000) + (timePassedMinutes * 1000 * 60) + (timePassedHours * 1000 * 3600)
-				timePassed := timePassedMs * 1000000
+				timeCode := (timeCodeMs - offSetMs) * 1000000
 				// Wait until time to print
-				for timePassed > uint64(time.Since(timeStart)) {
+				for timeCode > int64(time.Since(timeStart)) {
 					time.Sleep(sleepTimeMs * time.Millisecond)
 				}
 
 				// Fetch the line
-				outText := reLine.ReplaceAllString(foundLine, "$5")
+				outText := reLine.ReplaceAllString(foundLine, "$3")
 
 				fmt.Fprintf(writer, "%s\n", outText)
 			} else {
-				timePassedMs := time.Since(timeStart) / 1000000
-				timePassedMilliSeconds := timePassedMs % 100
-				timePassedSeconds := (timePassedMs / 1000) % 60
-				timePassedMinutes := (timePassedMs / (60 * 1000)) % 60
-				timePassedHours := timePassedMs / (60 * 60 * 1000)
-				fmt.Fprintf(writer, "%02d:%02d:%02d.%03d:%s", timePassedHours, timePassedMinutes, timePassedSeconds, timePassedMilliSeconds, text)
+				timeCodeMs := int64((time.Since(timeStart) / 1000000)) + offSetMs
+				if timeCodeMs < 0 {
+					timeCodeMs = -timeCodeMs
+					fmt.Fprintf(writer, "-")
+				}
+				timeCodeMilliSeconds := timeCodeMs % 100
+				timeCodeSeconds := (timeCodeMs / 1000) % 60
+				timeCodeMinutes := (timeCodeMs / (60 * 1000)) % 60
+				timeCodeHours := timeCodeMs / (60 * 60 * 1000)
+				fmt.Fprintf(writer, "%02d:%02d:%02d.%03d:%s", timeCodeHours, timeCodeMinutes, timeCodeSeconds, timeCodeMilliSeconds, text)
 			}
 			writer.Flush()
 		}
